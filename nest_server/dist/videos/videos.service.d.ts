33ad21b58255a0b167e2c256012efc87310ea999
Nickolay <mlhv.n@doubletapp.io>
2023-05-23 12:30:58 +0500

    Removed extra dir

--- nest_server/dist/videos/videos.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { CreateVideoDto } from './dto/create-video.dto';
-import { UpdateVideoDto } from './dto/update-video.dto';
-export declare class VideosService {
-    create(createVideoDto: CreateVideoDto): string;
-    findAll(): string;
-    findOne(id: number): string;
-    update(id: number, updateVideoDto: UpdateVideoDto): string;
-    remove(id: number): string;
-}
